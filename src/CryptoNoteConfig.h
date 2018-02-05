3d0ed77ad69988c6364fd9c7549efce36b54d75a
Harsha <harsha.anegondi@gmail.com>
2018-02-05 15:22:58 -0700

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -108,7 +108,7 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 const std::initializer_list<const char*> SEED_NODES = {
   //"your_seed_ip1.com:8080",
   //"your_seed_ip2.com:8080",
-  "jarviscoin.network:81021",
+  "45.32.74.117:81021",
 };
 
 struct CheckpointData {
